8aeb0fe25ad3ec9efaf3cdc71746d14b5d79f6e0
Santiago Fernandez <t-safer@microsoft.com>
2015-05-28 11:08:10 -0700

    Added ILDasm and Readers static class to keep peReader and mdReader alive.

--- src/ILDasmLibrary/ILDasmLibrary/ILDasm.cs
+++ src/ILDasmLibrary/ILDasmLibrary/ILDasm.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.File;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +12,43 @@ namespace ILDasmLibrary
 {
     public class ILDasm
     {
-        private PEReader _peReader;
-        
+
+        public ILDasm(Stream fileStream)
+        {
+            Readers.PeReader = new PEReader(fileStream);
+        }
+
+        public ILDasm(string path)
+            :this(File.OpenRead(path))
+        {   
+        }
     }
+
+    internal static class Readers{
+        private static PEReader _peReader;
+        private static MetadataReader _mdReader;
+        
+        public static PEReader PeReader {
+            get
+            {
+                return _peReader;
+            }
+            internal set
+            {
+                _peReader = value;
+            }
+        }
+
+        public static MetadataReader MdReader
+        {
+            get
+            {
+                if(_peReader == null)
+                {
+                    throw new Exception("PEReader has to be initialized before trying to get MdReader");
+                }
+                return _peReader.GetMetadataReader();
+            }
+        }
+   }
 }
